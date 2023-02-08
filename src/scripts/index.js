441478b98d762cfbf771418af1af0a23a8db0d49
Konstantin <38990885+konstantingalin@users.noreply.github.com>
2023-02-08 23:59:04 +0600

    for socials club

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,30 +9,37 @@ window.addEventListener('DOMContentLoaded', function () {
 	var score = 0;
     var time = 30;
     var gameObjects = [];
-    var maxObjects = 10;
+    var maxObjects = 25;
     var gameBoard = document.getElementById("game-board");
 
     function updateScore() {
-      document.getElementById("score").innerHTML = "Очки: " + score;
+      document.getElementById("score").innerHTML = score;
     }
 
     function startTimer() {
       var timer = setInterval(function() {
         time--;
-        document.getElementById("timer").innerHTML = "Времени осталось: " + time;
+        document.getElementById("timer").innerHTML = time;
         if (time === 0) {
           clearInterval(timer);
-          alert("Time's up! Your final score is: " + score);
+          
+          document.querySelector('._2').style.display = 'none';
+          document.querySelector('._3').style.display = 'block';
+          document.querySelector('#score_result').textContent = score;
+          
         }
       }, 1000);
     }
 
     function createGameObject() {
-      var x = Math.floor(Math.random() * 700);
-      var y = Math.floor(Math.random() * 664);
+      var x = Math.floor(Math.random() * (document.querySelector('#game-board').offsetWidth));
+      var y = Math.floor(Math.random() * window.innerHeight);
+      console.log(document.querySelector('.game_content').offsetWidth);
       var obj_img = 'game-object' + (Math.floor(Math.random() * 3) + '');
+      var obj_anm = 'animation_' + (Math.floor(Math.random() * 8) + '');
       var object = document.createElement("div");
       object.classList.add(obj_img);
+      object.classList.add(obj_anm);
       object.style.left = x + "px";
       object.style.top = y + "px";
       object.addEventListener("click", function() {
@@ -52,5 +59,18 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     }
 
-	startTimer(); startGame()
+  this.document.querySelector('.button_start').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    this.document.querySelector('._1').style.display = 'none';
+    this.document.querySelector('._2').style.display = 'block';
+
+    startTimer(); startGame()
+  })
+
+  this.document.querySelector('.button_reset').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    document.location.reload();
+  })
 });
